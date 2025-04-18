577a01af6c31e0b58f441b3e9fa6f1da42e3d8fd
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2025-04-18 21:22:01 +1000

    Add enemy

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,7 +4,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("DEATH FOR YOU!")
+	Engine.time_scale = 0.2
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
