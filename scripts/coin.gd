06e8e13d6d16be75ba3c10ea9a2854483fbf9540
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2025-04-18 22:25:27 +1000

    Add GameManager and scoring

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,5 +16,5 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin!")
+	game_manager.add_point()
 	queue_free()
