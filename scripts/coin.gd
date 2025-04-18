ff6d2b9cc00aa34742930a9250a9798f54fe8a38
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2025-04-18 22:51:24 +1000

    Add music sfx

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,20 +1,9 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-	# wtf does pass do?
-	# pass # Replace with function body.
-	#print("Im a coin")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup")
+	#queue_free() no need since called from animation
